512f92143e8ddf6caad8519c39e80d9e9830df9a
Mardoqueo Zotelo <mardoflorentin18@gmail.com>
2024-12-03 17:56:35 -0300

    pequeños ajustes en el pading del buscador de personajes

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -9,7 +9,7 @@ const Characters = () => {
             <div id="contenedor" className="min-h-screen flex flex-col items-center relative">
                 <h1 id="titulo" className="text-white pt-4 text-4xl">{t('title_char_list')}</h1>
                 <div className="w-full m-3 p-3">
-                    <Search className="w-full p-2 rounded-lg" placeholder={t('search_input')} />
+                    <Search className="w-full p-3 rounded-lg" placeholder={t('search_input')} />
                 </div>
                 <Card />
             </div>
